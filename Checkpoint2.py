fa7b75e60f67464b8ba74804d09e4e2b459a9570
OtavioBtm1 <rm5502012@fiap.com.br>
2023-05-03 20:44:41 -0300

    Adição de um README e de um sistema de estoque - Otavio Vitoriano

--- Checkpoint2.py
+++ Checkpoint2.py
@@ -3,36 +3,81 @@
 # Gabriel Torres 98600
 # Jéssica Brum 97944
 # Eduardo Fedeli 550132
-######################### CADASTRO DE CLIENTE# #############################
+# cria um dicionário vazio para representar o estoque
+
+estoque = {"vinho tinto": 10, "vinho branco": 5, "espumante": 8}  # Dicionário para controle de estoque
+precos = {"vinho tinto": 50.0, "vinho branco": 40.0, "espumante": 70.0}  # Dicionário para controle de preços
+
 print("Seja bem-vindo à Vinheria Agnello!")
-nome = input("Digite seu primeiro nome: ")
-sobrenome = input("Digite seu sobrenome: ")
-idade = int(input("Digite sua idade: "))
+print("Estes são os produtos disponíveis e seus respectivos preços:")
+for produto, preco in precos.items():
+    print(f"- {produto.capitalize()}: R$ {preco:.2f}")
 
-print("\nAgora vamos para o cadastro de endereço!")
+while True:
+    # Loop para receber as compras do usuário
+    compra = input("\nDigite o produto que deseja comprar ou 'sair' para encerrar: ").lower()
 
-rua = None 
-while rua is None:
-    rua = input("Digite o nome da sua rua: ")
+    if compra == "sair":
+        print("\nObrigado por comprar na Vinheria Agnello!")
+        break
 
-bairro = None
-while bairro is None:
-    bairro = input("Digite o bairro: ")
+    # Verifica se o produto está disponível no estoque
+    if compra not in estoque:
+        print("Desculpe, produto não disponível.")
+        continue
 
-num = None
-while num is None:
-    num = input("Digite o número da casa: ")
+    quantidade = int(input(f"Digite a quantidade de {compra.capitalize()} que deseja comprar: "))
+    if quantidade > estoque[compra]:
+        print(f"Desculpe, não temos {quantidade} unidades de {compra.capitalize()} em estoque.")
+        continue
 
-cep = None
-cep_str = input("Digite o CEP: ")
-try:
-    cep = int(cep_str)
-except ValueError:
-        print("Por favor, digite um CEP válido.")
+    # Calcula o valor total da compra
+    valor_total = quantidade * precos[compra]
+    print(f"O valor total da sua compra de {quantidade} unidades de {compra.capitalize()} é de R$ {valor_total:.2f}")
 
-comp = None
-while comp is None:
-    comp = input("Digite o complemento (opcional): ") or None
+    # Atualiza o estoque
+    estoque[compra] -= quantidade
+
+
+
+######################### CADASTRO DE CLIENTE# #############################
+pos=["sim", "Sim", "S", "s", "Ss", "ss"]
+
+print("Seja bem-vindo à Vinheria Agnello!")
+dec = None
+while dec is None:
+    dec=input("Você deseja realizar uma compra? Se sim, vamos para o cadastro!")
+    if dec in pos:
+        nome = input("Digite seu primeiro nome: ")
+        sobrenome = input("Digite seu sobrenome: ")
+        idade = int(input("Digite sua idade: "))
+
+        print("\nAgora vamos para o cadastro de endereço!")
+
+        rua = None 
+        while rua is None:
+            rua = input("Digite o nome da sua rua: ")
+
+        bairro = None
+        while bairro is None:
+            bairro = input("Digite o bairro: ")
+
+        num = None
+        while num is None:
+            num = input("Digite o número da casa: ")
+
+        cep = None
+        cep_str = input("Digite o CEP: ")
+        try:
+            cep = int(cep_str)
+        except ValueError:
+                print("Por favor, digite um CEP válido.")
+
+        comp = None
+        while comp is None:
+            comp = input("Digite o complemento (opcional): ") or None
+    else:
+        print("Obrigado, tenha um bom dia!")
 ######################### CADASTRO DE CLIENTE# #############################
 
 
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # CP_Python
 Checkpoint 2 - Python 
+
+# VinheriaAgnello - Controle de Compras e Estoque
+
+## Descrição
+Este programa tem como objetivo auxiliar a Vinheria Agnello no controle de suas compras de suprimentos e no gerenciamento de seu estoque. Com ele, será possível cadastrar as compras realizadas, controlar as entradas e saídas de produtos do estoque, além de verificar o estoque atual de cada item.
+
+## Funcionalidades
+O programa conta com dois menus principais: Compras e Estoque.
+
+### Compras
+No menu de Compras, é possível realizar as seguintes operações:
+- Cadastrar nova compra: permite adicionar uma nova compra ao sistema, informando a descrição do item, a quantidade adquirida e o valor unitário. O programa irá calcular o valor total da compra e atualizar o estoque.
+- Mostrar todas as compras: exibe um relatório com todas as compras realizadas, incluindo a descrição do item, a quantidade, o valor unitário, o valor total e a data da compra.
+
+### Estoque
+No menu de Estoque, é possível realizar as seguintes operações:
+- Registrar entrada de produto: permite adicionar uma nova entrada de produto no estoque, informando a descrição do item e a quantidade recebida.
+- Registrar saída de produto: permite registrar a venda de um produto, informando a descrição do item e a quantidade vendida. O programa irá calcular o valor total da venda e atualizar o estoque.
+- Mostrar estoque atual: exibe um relatório com o estoque atual de todos os produtos, incluindo a descrição do item e a quantidade disponível.
+
+## Requisitos
+Para o programa funcionar corretamente, é necessário ter instalado em seu computador uma versão recente do Python (recomenda-se a versão 3.6 ou superior). Além disso, é preciso baixar o arquivo "VinheriaAgnello.py" e executá-lo em um terminal.
+
+## Como executar
+Para executar o programa, basta abrir um terminal e navegar até a pasta onde o arquivo "VinheriaAgnello.py" foi salvo. Em seguida, digite o comando "python VinheriaAgnello.py" e pressione Enter. O programa irá rodar e exibir o menu principal.
+
+## Observações
+- Todos os blocos do código estão comentados para facilitar a compreensão e a manutenção do programa.
+- O programa utiliza estruturas condicionais, laços de repetição, listas e estruturas Match-Case para realizar as operações solicitadas.
+- O CNPJ dos fornecedores não é armazenado no sistema, pois não é necessário para o controle de compras e estoque.
