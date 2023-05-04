87187be5c1ba351ca8f47229f1307c5f4b3a8342
OtavioBtm1 <rm5502012@fiap.com.br>
2023-05-03 21:18:53 -0300

    Edição de indentação do código

--- Checkpoint2.py
+++ Checkpoint2.py
@@ -5,40 +5,6 @@
 # Eduardo Fedeli 550132
 # cria um dicionário vazio para representar o estoque
 
-estoque = {"vinho tinto": 10, "vinho branco": 5, "espumante": 8}  # Dicionário para controle de estoque
-precos = {"vinho tinto": 50.0, "vinho branco": 40.0, "espumante": 70.0}  # Dicionário para controle de preços
-
-print("Seja bem-vindo à Vinheria Agnello!")
-print("Estes são os produtos disponíveis e seus respectivos preços:")
-for produto, preco in precos.items():
-    print(f"- {produto.capitalize()}: R$ {preco:.2f}")
-
-while True:
-    # Loop para receber as compras do usuário
-    compra = input("\nDigite o produto que deseja comprar ou 'sair' para encerrar: ").lower()
-
-    if compra == "sair":
-        print("\nObrigado por comprar na Vinheria Agnello!")
-        break
-
-    # Verifica se o produto está disponível no estoque
-    if compra not in estoque:
-        print("Desculpe, produto não disponível.")
-        continue
-
-    quantidade = int(input(f"Digite a quantidade de {compra.capitalize()} que deseja comprar: "))
-    if quantidade > estoque[compra]:
-        print(f"Desculpe, não temos {quantidade} unidades de {compra.capitalize()} em estoque.")
-        continue
-
-    # Calcula o valor total da compra
-    valor_total = quantidade * precos[compra]
-    print(f"O valor total da sua compra de {quantidade} unidades de {compra.capitalize()} é de R$ {valor_total:.2f}")
-
-    # Atualiza o estoque
-    estoque[compra] -= quantidade
-
-
 
 ######################### CADASTRO DE CLIENTE# #############################
 pos=["sim", "Sim", "S", "s", "Ss", "ss"]
@@ -75,8 +41,45 @@ while dec is None:
 
         comp = None
         while comp is None:
+        
             comp = input("Digite o complemento (opcional): ") or None
-    else:
+
+##########################SISTEMA DE ESTOQUE E COMPRA#################
+estoque = {"vinho tinto": 10, "vinho branco": 5, "espumante": 8}  # Dicionário para controle de estoque
+precos = {"vinho tinto": 50.0, "vinho branco": 40.0, "espumante": 70.0}  # Dicionário para controle de preços
+
+print("Seja bem-vindo à Vinheria Agnello!")
+print("Estes são os produtos disponíveis e seus respectivos preços:")
+for produto, preco in precos.items():
+    print(f"- {produto.capitalize()}: R$ {preco:.2f}")
+
+while True:
+    # Loop para receber as compras do usuário
+    compra = input("\nDigite o produto que deseja comprar ou 'sair' para encerrar: ").lower()
+
+    if compra == "sair":
+        print("\nObrigado por comprar na Vinheria Agnello!")
+        break
+
+    # Verifica se o produto está disponível no estoque
+    if compra not in estoque:
+        print("Desculpe, produto não disponível.")
+        continue
+
+    quantidade = int(input(f"Digite a quantidade de {compra.capitalize()} que deseja comprar: "))
+    if quantidade > estoque[compra]:
+        print(f"Desculpe, não temos {quantidade} unidades de {compra.capitalize()} em estoque.")
+        continue
+
+    # Calcula o valor total da compra
+    valor_total = quantidade * precos[compra]
+    print(f"O valor total da sua compra de {quantidade} unidades de {compra.capitalize()} é de R$ {valor_total:.2f}")
+
+    # Atualiza o estoque
+    estoque[compra] -= quantidade
+##########################SISTEMA DE ESTOQUE E COMPRA#################
+
+else:
         print("Obrigado, tenha um bom dia!")
 ######################### CADASTRO DE CLIENTE# #############################
 
